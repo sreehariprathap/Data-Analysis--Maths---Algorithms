bcfc2fd8ab24a31286e04850a56da5e065770f6d
Sreehari Prathap <sreehariprathap1996@gmail.com>
2024-09-12 10:49:25 -0400

    added week 2 data

---
+++ Week-2/Notebook/PythonMathScript.py
@@ -0,0 +1,18 @@
+import sympy as sp
+
+# Basic Math Operations
+a = 10
+b = 5
+
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+exponentiation = a ** b
+
+print("Basic Math Operations:")
+print(f"Addition: {a} + {b} = {addition}")
+print(f"Subtraction: {a} - {b} = {subtraction}")
+print(f"Multiplication: {a} * {b} = {multiplication}")
+print(f"Division: {a} / {b} = {division}")
+print(f"Exponentiation: {a} ** {b} = {exponentiation}")
